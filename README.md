c93c56010ddd32913692bf7febfa35ab63286f8a
Unbot2313 <126409052+Unbot2313@users.noreply.github.com>
2024-12-05 15:42:25 -0500

    feature: endpoint for get static files (.ts and m3u8), fix: added details in readme.md

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 \*\* Para recargar la documentacion usar `swag init` en consola
+
+// comando para usar ffmpeg
+// `wsl ffmpeg -i /mnt/c/Users/tomas/Desktop/GithubProyects/UnbotProyects/linklnProyects/go-streaing-service/videos/4171d0aa-d89c-4ce1-a8ed-e8fe35eb0df8_badApple!.mp4 -c copy -start_number 0 -hls_time 10 -hls_list_size 0 -f hls /mnt/c/Users/tomas/Desktop/GithubProyects/UnbotProyects/linklnProyects/go-streaing-service/public/output.m3u8`
--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+
 	swaggerfiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
 	_ "github.com/unbot2313/go-streaming-service/docs"
@@ -20,9 +22,17 @@ func main() {
 
 	r := gin.Default()
 
+	r.Use(cors.Default()) // Habilita CORS para todos los orígenes
+
 	apiGroup := r.Group("/api")
 
 	v1Group := apiGroup.Group("/v1")
+
+	// Servir archivos staticos (STREAMING)
+	// cuando se accede a la ruta /static, se sirven los archivos que estan en la carpeta public,
+	// ejm: http://localhost:3003/static/index.html, se sirve /public/index.html
+	v1Group.Static("/static", "./public")
+
 
 	// Inicializar los componentes de la aplicación
 	userController, authController, videoController := app.InitializeComponents()
